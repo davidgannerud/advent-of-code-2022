2ba95e969e3f239d038bc1bf5a207158ec762b43
David Gannerud <david@dkrg.se>
2022-12-04 07:45:24 +0100

    Add a simple template file

---
+++ template.py
@@ -0,0 +1,17 @@
+input_file = 'test.txt'
+# Part 1
+res = 0
+with open(input_file) as f:
+    for line in f:
+        line = line.strip()
+
+print("Part 1:", res)
+print("\n")
+
+# Part 2
+res = 0
+with open(input_file) as f:
+    for line in f:
+        line = line.strip()
+
+print("Part 2:", res)
